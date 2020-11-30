2bc9b9eb320e0311e676606915c8b8d6d81dd986
Aggiepolo08 <73924964+Aggiepolo08@users.noreply.github.com>
2020-11-29 18:24:03 -0600

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,25 +1,63 @@
 def on_button_pressed_a():
     global RunProject
-    test = 0
-    serial.write_line("" + str((test)))
     basic.show_icon(IconNames.YES)
     RunProject = True
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    global RunProject
+    global RunProject, redValue, blueValue, greenValue
     RunProject = False
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    redValue = -1
+    blueValue = -1
+    greenValue = -1
     basic.show_icon(IconNames.NO)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+greenValue = 0
+blueValue = 0
+redValue = 0
 RunProject = False
 BlueButton = -1
 RunProject = False
+redValue = -1
+blueValue = -1
+greenValue = -1
 
 def on_forever():
-    global BlueButton
+    global BlueButton, redValue, blueValue, greenValue
     while RunProject == True:
         basic.show_icon(IconNames.HEART)
         BlueButton = pins.digital_read_pin(DigitalPin.P5)
-        serial.write_line("BlueButton: " + str((BlueButton)))
+        if BlueButton == 0:
+            pins.digital_write_pin(DigitalPin.P8, 1)
+            pins.digital_write_pin(DigitalPin.P12, 0)
+        else:
+            pins.digital_write_pin(DigitalPin.P8, 0)
+            pins.digital_write_pin(DigitalPin.P12, 0)
+            redValue = envirobit.get_red()
+            blueValue = envirobit.get_blue()
+            greenValue = envirobit.get_green()
+            # Red
+            if 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # Blue
+            elif 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # Green
+            elif 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # Yellow
+            elif 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # Black
+            elif 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # White
+            elif 185 >= redValue and redValue >= 151 and (57 >= blueValue and blueValue >= 35) and (56 >= greenValue and greenValue >= 38):
+                pass
+            # Unknown
+            else:
+                pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,61 @@
 input.onButtonPressed(Button.A, function () {
-    let test = 0
-    serial.writeLine("" + test)
     basic.showIcon(IconNames.Yes)
     RunProject = true
 })
 input.onButtonPressed(Button.B, function () {
     RunProject = false
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    redValue = -1
+    blueValue = -1
+    greenValue = -1
     basic.showIcon(IconNames.No)
 })
+let greenValue = 0
+let blueValue = 0
+let redValue = 0
 let RunProject = false
 let BlueButton = -1
 RunProject = false
+redValue = -1
+blueValue = -1
+greenValue = -1
 basic.forever(function () {
     while (RunProject == true) {
         basic.showIcon(IconNames.Heart)
         BlueButton = pins.digitalReadPin(DigitalPin.P5)
-        serial.writeLine("BlueButton: " + ("" + BlueButton))
+        if (BlueButton == 0) {
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+        } else {
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            redValue = envirobit.getRed()
+            blueValue = envirobit.getBlue()
+            greenValue = envirobit.getGreen()
+            // Red
+            // Blue
+            // Green
+            // Yellow
+            // Black
+            // White
+            // Unknown
+            if (185 >= redValue && redValue >= 151 && (57 >= blueValue && blueValue >= 35) && (56 >= greenValue && greenValue >= 38)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else if (66 >= redValue && redValue >= 21 && (147 >= blueValue && blueValue >= 108) && (79 >= greenValue && greenValue >= 68)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else if (104 >= redValue && redValue >= 31 && (76 >= blueValue && blueValue >= 59) && (137 >= greenValue && greenValue >= 80)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else if (255 >= redValue && redValue >= 102 && (101 >= blueValue && blueValue >= 32) && (255 >= greenValue && greenValue >= 78)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else if (92 >= redValue && redValue >= 63 && (89 >= blueValue && blueValue >= 72) && (103 >= greenValue && greenValue >= 77)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else if (255 >= redValue && redValue >= 104 && (255 >= blueValue && blueValue >= 116) && (255 >= greenValue && greenValue >= 104)) {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            } else {
+                pins.servoWritePin(AnalogPin.P7, 0)
+            }
+            pins.servoWritePin(AnalogPin.P9, 100)
+        }
     }
 })
